94499d342c8196e1aedf1f0acb65bcaa60d95d0d
DAMISA <damisa.alex@gmail.com>
2022-07-23 05:16:39 +0100

    clean up1

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ app.get('/weather', (req, res) => {
 app.get('/products', (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: 'You must provide a search term',
+      error: 'Please provide a search term',
     });
   }
 
